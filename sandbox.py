846766e44d7df04f5fceb810a233ec4fe80f73ae
Jakub Domanski <jakub.piotr.domanski@gmail.com>
2023-10-18 14:08:49 +0200

    reduced size for ASCII

--- sandbox.py
+++ sandbox.py
@@ -23,10 +23,17 @@ class Screen():
     def project(self, coords):
         for point in coords:
             point_z, point_b = point[2], point[3]
-            pos = [int(point[i] * self.camera_dist / point_z) + self.center[i] for i in (1, 0)]
-            # axes need to be swapped for plotting
+            pos = []
 
-            if point_z < self.z_buffer[*pos]:
+            for i in (1, 0): # axes need to be swapped for plotting
+                screen_coord = int(point[i] * self.camera_dist / point_z)
+                if abs(screen_coord) < int(self.size[i] / 2):
+                    pos.append(screen_coord + self.center[i])
+                else: # projection beyond the screen
+                    pos = []
+                    break
+                
+            if pos and point_z < self.z_buffer[*pos]:
                 self.screen.itemset(*pos, point_b)
                 self.z_buffer.itemset(*pos, point_z)
 
@@ -47,59 +54,14 @@ def rot(axis, t, vector):
 
     return np.matmul(matrix, vector)
 
-screen = Screen((500, 500), 200)
+screen = Screen((64, 64), 100)
 
-# ellipse
-# shape = [np.array([200*np.sin(t), 500*np.cos(t), 6]) for t in np.linspace(0, 2*np.pi, 1000)]
-
-# rails
-# shape = [np.array([-200, -200, 200+t]) for t in np.linspace(0, 500, 1000)] + [np.array([200, -200, 200+t]) for t in np.linspace(0, 500, 1000)]
-
-# tilted square
-# shape = [np.array([x, y, 10+0.01*y]) for x in np.linspace(-300, 300, 500) for y in np.linspace(0, 300, 500)]
-
-# circle by rotation
-# shape = []
-# for t in np.linspace(0, 2*np.pi, 500):
-#     rot = np.array([[np.cos(t), -np.sin(t), 0], [np.sin(t), np.cos(t), 0], [0, 0, 1]])
-#     init_vector = np.array([300, 300, 10])
-#     shape.append(np.matmul(rot, init_vector))
-
-# sphere
-# shape = []
-# vector = np.array([200, 0, 0])
-# offset = np.array([0, 0, 300])
-# for t in np.linspace(0, 2*np.pi, 1000):
-#     rot_z = np.array([[np.cos(t), -np.sin(t), 0], [np.sin(t), np.cos(t), 0], [0, 0, 1]])
-#     for b in np.linspace(0, np.pi, 1000):
-#         rot_y = np.array([[np.cos(b), 0, np.sin(b)], [0, 1, 0], [-np.sin(b), 0, np.cos(b)]])
-#         shape.append(np.matmul(rot_y, np.matmul(rot_z, vector)) + offset)
-
-# cylinder
-# shape = []
-# vector = np.array([200, 0, 0])
-# offset = np.array([0, -100, 300])
-# for b in np.linspace(0, 2*np.pi, 500):
-#     rot_y = np.array([[np.cos(b), 0, np.sin(b)], [0, 1, 0], [-np.sin(b), 0, np.cos(b)]])
-#     for dy in np.linspace(0, -20, 500):
-#         shape.append(np.matmul(rot_y, vector) + [0, dy, 0] + offset)
-
-# torus
-# shape = []
-# radius = np.array([50, 0, 0])
-# offset = np.array([0, 0, 250])
-# for a in np.linspace(0, 2*np.pi, 100):
-#     for b in np.linspace(0, 2*np.pi, 100):
-#         v = rot('z', a, radius)
-#         v = rot('y', b, v + [100, 0, 0])
-#         v = rot('x', -np.pi/4, v)
-#         shape.append(v + offset)
 
 def transform(vector):
     return rot('x', -np.pi/4, vector)
 
-R_val = 100
-r_val = 50
+R_val = 50
+r_val = 20
 
 offset = np.array([0, 0, 250])
 light=np.array([0, -1, 0])
@@ -108,10 +70,10 @@ shape = []
 R0 = np.array([R_val, 0, 0])
 r0 = np.array([0, r_val, 0])
 
-for a in np.linspace(0, 2*np.pi, 500):
+for a in np.linspace(0, 2*np.pi, 256):
     r = rot('z', a, r0)
 
-    for b in np.linspace(0, 2*np.pi, 500):
+    for b in np.linspace(0, 2*np.pi, 256):
         v = rot('y', b, r + R0)
         Tr = transform(rot('y', b, r))
         brightness = - np.dot(unit(light), unit(Tr))
